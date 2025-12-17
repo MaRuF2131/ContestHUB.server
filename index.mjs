fdcf0cb9f3608227e24c0e05e6ab393e119b2ebe
MaRuF2131 <marufahmmed916@gmail.com>
2025-12-17 21:32:51 +0600

    setup

---
+++ index.mjs
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import mongo from './MongoDB.mjs';
+dotenv.config();
+const app = express();
+let db;
+(async () => {
+  try {
+
+    db = await mongo()
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err);
+  }
+})();
+
+app.use(cookieParser());
+app.use(cors({origin:['http://localhost:5173'],credentials:true ,allowedHeaders: ['Content-Type', 'Authorization', 'x-user','authorization']}));
+app.use(express.json());
+
+
+
+
+app.get('/',async (req ,res)=>{
+    res.send('Hello World');
+
+});
+
+
+ app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`); 
+}); 
+
+export default app;
