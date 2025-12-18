670914d100d7a0eab3d0d5c2fc1244e7a9f8e904
MaRuF2131 <marufahmmed916@gmail.com>
2025-12-18 15:43:49 +0600

    config set

---
+++ config/security.config.mjs
@@ -0,0 +1,17 @@
+import helmet from "helmet";
+import cors from "cors";
+import { sanitizeMiddleware } from "../utils/sanitizer";
+import { apiLimiter } from "../middlewares/rateLimit.middleware";
+
+export const security = (app) => {
+  app.use(apiLimiter)
+  app.use(helmet());
+
+  app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      credentials: true,
+    })
+  );
+  app.use(sanitizeMiddleware());
+};
